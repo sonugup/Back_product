a5d5247c6938a4a1048cd42299945404a0ff1a7e
sonugup <skg30336@gmail.com>
2024-01-27 19:40:31 +0530

    backend deploy

--- Controlar/User.Controler.js
+++ Controlar/User.Controler.js
@@ -1,19 +1,17 @@
 const jwt = require("jsonwebtoken");
-const bcrypt=require("bcrypt")
-const {UserModel}=require("../Models/User.model");
-const dotenv=require("dotenv")
+const bcrypt = require("bcrypt")
+const { UserModel } = require("../Models/User.model");
+const dotenv = require("dotenv")
 dotenv.config();
-const creatRegister=  async (req, res) => {
-
-
-    const {name, email, pass} = req.body;
-    const exstEmail= await UserModel.findOne({email})
-    const lPass= await bcrypt.hash(pass, 6)
-    if(exstEmail){
+const creatRegister = async (req, res) => {
+    const { name, email, pass } = req.body;
+    const exstEmail = await UserModel.findOne({ email })
+    const lPass = await bcrypt.hash(pass, 6)
+    if (exstEmail) {
         res.json("email already exist")
-    }else{
+    } else {
         try {
-            const user = new UserModel({name, email, pass:lPass})
+            const user = new UserModel({ name, email, pass: lPass })
             await user.save()
             res.json(user)
         } catch (err) {
@@ -21,26 +19,23 @@ const creatRegister=  async (req, res) => {
             res.json("err some")
         }
     }
-
-   
-
 }
 
-const creatLogin=async (req, res) => {
+const creatLogin = async (req, res) => {
     const { email, pass } = req.body;
     try {
         const user = await UserModel.find({ email })
         if (user.length > 0) {
-            bcrypt.compare(pass, user[0].pass,(err, result) => {
-                if(result){
+            bcrypt.compare(pass, user[0].pass, (err, result) => {
+                if (result) {
                     const token = jwt.sign({ userID: user[0]._id }, process.env.security);
                     res.json({ msg: "Login", token: token })
 
-                }else{
+                } else {
                     res.json("somethig going wrong in hash")
                 }
-            } )
-            
+            })
+
         } else {
             res.json("Wrong Credntials")
         }
@@ -50,7 +45,7 @@ const creatLogin=async (req, res) => {
     }
 }
 
-module.exports={
+module.exports = {
     creatRegister,
     creatLogin
 }
